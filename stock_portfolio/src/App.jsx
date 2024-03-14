32b3266ea838e3bcc2c8af53e24e66bddd8879ba
Jun <jxiu_91@hotmail.com>
2024-03-14 13:42:29 +0800

    initial commit, add process stock data button

---
+++ stock_portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import StockDataComponent from "./components/StockDataComponent";
+
+function App() {
+    return (
+        <div>
+            <h2>CHECK FOR DATA FROM API</h2>
+            <StockDataComponent></StockDataComponent>
+        </div>
+    );
+}
+
+export default App;
